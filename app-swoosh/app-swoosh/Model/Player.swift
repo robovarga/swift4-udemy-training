9ad734295991d2e2099c0fd64bea89c3dac66ae3
Robert Varga <robovarga.com@gmail.com>
2017-10-16 21:16:49 +0200

    app-swoosh - Added player model

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Robert Varga on 16/10/2017.
+//  Copyright © 2017 Robert Varga. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var dessiredLeague: String?
+    var selectedSkillLevel: String?
+}
